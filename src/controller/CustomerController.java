7412b0b3c76e2028d5bf73f4433018dfcb30cc2f
AkinduRamsith <akiyaramsith2002@gmail.com>
2023-08-09 16:57:59 +0530

    Day 15

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -3,254 +3,119 @@ package controller;
 import db.DBConnection;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
 import model.Customer;
-import model.Item;
 
-import java.net.URL;
 import java.sql.*;
-import java.util.ResourceBundle;
 
-public class CustomerController implements Initializable {
-    public TextField txtId;
-    public TextField txtName;
-    public TextField txtAddress;
-    public TextField txtSalary;
-
-    @FXML
-    private TableColumn<Customer, String> colAddress;
-
-    @FXML
-    private TableColumn<Customer, String> colID;
-
-    @FXML
-    private TableColumn<Customer, String> colName;
-
-    @FXML
-    private TableColumn<Customer, Double> colSalary;
-
-    @FXML
-    private TableView<Customer> tblCustomer;
-
-
+public class CustomerController implements CustomerService{
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        setCellValueFactory();
+    public boolean addCustomer(Customer customer) {
         try {
-            loadTable();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        tblCustomer.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if (null != newValue) {
-                setTableValues(newValue);
-            }
-        });
-    }
-
-    public static ObservableList<String> getAllCustomerId() throws SQLException, ClassNotFoundException {
-        ResultSet rst = DBConnection.getInstance().getConnection().prepareStatement("Select id From Customer").executeQuery();
-        ObservableList<String> list = FXCollections.observableArrayList();
-        while (rst.next()) {
-            list.add(rst.getString("id"));
-        }
-        return list;
-    }
-
-    public static Customer getAllCustomers(String id) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getInstance().getConnection();
-        Statement stm = connection.createStatement();
-        String SQL = "Select * From Customer where id='" + id + "'";
-        ResultSet rst = stm.executeQuery(SQL);
-        if (rst.next()) {
-            return new Customer(id, rst.getString("name"), rst.getString("address"), rst.getDouble("salary"));
-        }
-
-        return null;
-    }
-
-    private void setTableValues(Customer newValue) {
-        txtId.setText(newValue.getId());
-        txtName.setText(newValue.getName());
-        txtAddress.setText(newValue.getAddress());
-        txtSalary.setText(String.valueOf(newValue.getSalary()));
-    }
-
-    private void loadTable() throws SQLException, ClassNotFoundException {
-        String SQL = "Select * From Customer";
-        ObservableList<Customer> list = FXCollections.observableArrayList();
-        Connection connection = DBConnection.getInstance().getConnection();
-        PreparedStatement pstm = connection.prepareStatement(SQL);
-        ResultSet rst = pstm.executeQuery();
-
-        while (rst.next()) {
-            Customer customer = new Customer(rst.getString("id"), rst.getString("name"), rst.getString("address"), rst.getDouble("salary"));
-            list.add(customer);
-        }
-        tblCustomer.setItems(list);
-    }
-
-    private void setCellValueFactory() {
-        colID.setCellValueFactory(new PropertyValueFactory<Customer, String>("id"));
-        colName.setCellValueFactory(new PropertyValueFactory<Customer, String>("name"));
-        colAddress.setCellValueFactory(new PropertyValueFactory<Customer, String>("address"));
-        colSalary.setCellValueFactory(new PropertyValueFactory<Customer, Double>("salary"));
-    }
-
-    public void btnAddCustomer(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
-        try {
-            String id = txtId.getText();
-            String name = txtName.getText();
-            String address = txtAddress.getText();
-            double salary = Double.parseDouble(txtSalary.getText());
-            System.out.println(id + "," + name + "," + address + "," + salary);
-            Customer customer = new Customer(id, name, address, salary);
             String SQL = "Insert into Customer Values(?,?,?,?)";
-
             Connection connection = DBConnection.getInstance().getConnection();
             PreparedStatement pstm = connection.prepareStatement(SQL);
             pstm.setObject(1, customer.getId());
             pstm.setObject(2, customer.getName());
             pstm.setObject(3, customer.getAddress());
             pstm.setObject(4, customer.getSalary());
-            int i = pstm.executeUpdate();
-            if (i > 0) {
-                loadTable();
-                System.out.println("Added Success");
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Customer Add");
-                alert.setHeaderText(null);
-                alert.setContentText("Added Success");
 
-                alert.showAndWait();
-
-            } else {
-                System.out.println("Fail");
+            if(pstm.executeUpdate()>0){
+                return true;
             }
-
-
-            txtId.setText("");
-            txtName.setText("");
-            txtAddress.setText("");
-            txtSalary.setText("");
-        } catch (NumberFormatException ex) {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Error");
-            alert.setHeaderText(null);
-            alert.setContentText(ex.getMessage());
-
-            alert.showAndWait();
+        } catch (SQLException | ClassNotFoundException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+           // throw new RuntimeException(e);
         }
+
+        return false;
     }
 
-    public void btnSearchCustomer(ActionEvent actionEvent) {
+    @Override
+    public boolean updateCustomer(Customer customer) {
         try {
-            txtIdOnAction(actionEvent);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void btnUpdateCustomer(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
-        try {
-            String id = txtId.getText();
-            String name = txtName.getText();
-            String address = txtAddress.getText();
-            double salary = Double.parseDouble(txtSalary.getText());
-            Customer customer = new Customer(id, name, address, salary);
             String SQL = "Update Customer set name=?,address=?,salary=? where id=?";
             Connection connection = DBConnection.getInstance().getConnection();
             PreparedStatement pstm = connection.prepareStatement(SQL);
-            pstm.setObject(1, name);
-            pstm.setObject(2, address);
-            pstm.setObject(3, salary);
-            pstm.setObject(4, id);
-            int i = pstm.executeUpdate();
-            if (i > 0) {
-                loadTable();
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Update Success");
-                alert.setHeaderText(null);
-                alert.setContentText("Update Success");
-                alert.showAndWait();
+            pstm.setObject(1, customer.getName());
+            pstm.setObject(2, customer.getAddress());
+            pstm.setObject(3, customer.getSalary());
+            pstm.setObject(4, customer.getId());
+            if(pstm.executeUpdate()>0){
+                return true;
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+            // throw new RuntimeException(e);
+        }
 
+        return false;
+    }
 
-                txtId.setText("");
-                txtName.setText("");
-                txtAddress.setText("");
-                txtSalary.setText("");
+    @Override
+    public Customer searchCustomer(String id) {
+
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            Statement stm = connection.createStatement();
+            String SQL = "Select * From Customer where id='" + id + "'";
+            ResultSet rst = stm.executeQuery(SQL);
+            while(rst.next()){
+                Customer customer=new Customer(id,rst.getString("name"),rst.getString("address"),rst.getDouble("salary"));
+
+                    return customer;
 
             }
-        } catch (NumberFormatException ex) {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Error");
-            alert.setHeaderText(null);
-            alert.setContentText(ex.getMessage());
 
-            alert.showAndWait();
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+
         }
 
+        return null;
     }
 
-    public void btnDeleteCustomer(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+    @Override
+    public boolean deleteCustomer(String id) {
 
-        String SQL = "Delete From Customer where id='" + txtId.getText() + "'";
-        Connection connection = DBConnection.getInstance().getConnection();
-        Statement stm = connection.createStatement();
-        int i = stm.executeUpdate(SQL);
-        if (i > 0) {
-            loadTable();
-            System.out.println(i);
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Deleting");
-            alert.setHeaderText(null);
-            alert.setContentText("Delete Success");
-            alert.showAndWait();
 
-            txtId.setText("");
-            txtName.setText("");
-            txtAddress.setText("");
-            txtSalary.setText("");
-        } else {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Deleting");
-            alert.setHeaderText(null);
-            alert.setContentText("Empty Fields");
-
-            alert.showAndWait();
+        try {
+            String SQL = "Delete From Customer where id='" + id + "'";
+            Connection connection = DBConnection.getInstance().getConnection();
+            Statement stm = connection.createStatement();
+             if(stm.executeUpdate(SQL)>0){
+                 return true;
+             }
+        } catch (SQLException | ClassNotFoundException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+            // throw new RuntimeException(e);
         }
+
+        return false;
     }
 
-    public void txtIdOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getInstance().getConnection();
-        Statement stm = connection.createStatement();
-        String SQL = "Select * From Customer where id='" + txtId.getText() + "'";
-        ResultSet rst = stm.executeQuery(SQL);
-        if (rst.next()) {
-            txtId.setText(rst.getString("id"));
-            txtName.setText(rst.getString("name"));
-            txtAddress.setText(rst.getString("address"));
-            txtSalary.setText(String.valueOf(rst.getDouble("salary")));
-        } else {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Searching");
-            alert.setHeaderText(null);
-            alert.setContentText("Customer Not Found");
+    @Override
+    public ObservableList<Customer> getAllCustomer() {
 
-            alert.showAndWait();
+        try {
+            String SQL = "Select * From Customer";
+            ObservableList<Customer> list = FXCollections.observableArrayList();
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement pstm = connection.prepareStatement(SQL);
+            ResultSet rst = pstm.executeQuery();
+
+            while (rst.next()) {
+                Customer customer = new Customer(rst.getString("id"), rst.getString("name"), rst.getString("address"), rst.getDouble("salary"));
+                list.add(customer);
+
+            }
+            return list;
+
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
+
+        //return null;
     }
-
-
 }
---
+++ src/controller/CustomerFormController.java
@@ -0,0 +1,222 @@
+package controller;
+
+import db.DBConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Customer;
+
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
+
+public class CustomerFormController implements Initializable {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtSalary;
+
+    @FXML
+    private TableColumn<Customer, String> colAddress;
+
+    @FXML
+    private TableColumn<Customer, String> colID;
+
+    @FXML
+    private TableColumn<Customer, String> colName;
+
+    @FXML
+    private TableColumn<Customer, Double> colSalary;
+
+    @FXML
+    private TableView<Customer> tblCustomer;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setCellValueFactory();
+        try {
+            loadTable();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        tblCustomer.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (null != newValue) {
+                setTableValues(newValue);
+            }
+        });
+    }
+
+    public static ObservableList<String> getAllCustomerId() throws SQLException, ClassNotFoundException {
+        ResultSet rst = DBConnection.getInstance().getConnection().prepareStatement("Select id From Customer").executeQuery();
+        ObservableList<String> list = FXCollections.observableArrayList();
+        while (rst.next()) {
+            list.add(rst.getString("id"));
+        }
+        return list;
+    }
+
+    public static Customer getAllCustomers(String id) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        Statement stm = connection.createStatement();
+        String SQL = "Select * From Customer where id='" + id + "'";
+        ResultSet rst = stm.executeQuery(SQL);
+        if (rst.next()) {
+            return new Customer(id, rst.getString("name"), rst.getString("address"), rst.getDouble("salary"));
+        }
+
+        return null;
+    }
+
+    private void setTableValues(Customer newValue) {
+        txtId.setText(newValue.getId());
+        txtName.setText(newValue.getName());
+        txtAddress.setText(newValue.getAddress());
+        txtSalary.setText(String.valueOf(newValue.getSalary()));
+    }
+
+    private void loadTable() throws SQLException, ClassNotFoundException {
+       ObservableList<Customer> customerList=new CustomerController().getAllCustomer();
+        tblCustomer.setItems(customerList);
+    }
+
+    private void setCellValueFactory() {
+        colID.setCellValueFactory(new PropertyValueFactory<Customer, String>("id"));
+        colName.setCellValueFactory(new PropertyValueFactory<Customer, String>("name"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<Customer, String>("address"));
+        colSalary.setCellValueFactory(new PropertyValueFactory<Customer, Double>("salary"));
+    }
+
+    public void btnAddCustomer(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        try {
+            String id = txtId.getText();
+            String name = txtName.getText();
+            String address = txtAddress.getText();
+            double salary = Double.parseDouble(txtSalary.getText());
+            System.out.println(id + "," + name + "," + address + "," + salary);
+            Customer customer = new Customer(id, name, address, salary);
+            boolean isAdded=new CustomerController().addCustomer(customer);
+            if (isAdded) {
+                loadTable();
+                System.out.println("Added Success");
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Customer Add");
+                alert.setHeaderText(null);
+                alert.setContentText("Added Success");
+
+                alert.showAndWait();
+
+            }
+            txtId.setText("");
+            txtName.setText("");
+            txtAddress.setText("");
+            txtSalary.setText("");
+        } catch (NumberFormatException ex) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText(ex.getMessage());
+
+            alert.showAndWait();
+        }
+    }
+
+    public void btnSearchCustomer(ActionEvent actionEvent) {
+        try {
+            txtIdOnAction(actionEvent);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void btnUpdateCustomer(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        try {
+            String id = txtId.getText();
+            String name = txtName.getText();
+            String address = txtAddress.getText();
+            double salary = Double.parseDouble(txtSalary.getText());
+            Customer customer = new Customer(id, name, address, salary);
+            boolean isAdded=new CustomerController().updateCustomer(customer);
+
+            if (isAdded) {
+                loadTable();
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Update Success");
+                alert.setHeaderText(null);
+                alert.setContentText("Update Success");
+                alert.showAndWait();
+
+                txtId.setText("");
+                txtName.setText("");
+                txtAddress.setText("");
+                txtSalary.setText("");
+
+            }
+        } catch (NumberFormatException ex) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText(ex.getMessage());
+
+            alert.showAndWait();
+        }
+
+    }
+
+    public void btnDeleteCustomer(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        boolean isDeleted=new CustomerController().deleteCustomer(txtId.getText());
+        if (isDeleted) {
+            loadTable();
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Deleting");
+            alert.setHeaderText(null);
+            alert.setContentText("Delete Success");
+            alert.showAndWait();
+
+            txtId.setText("");
+            txtName.setText("");
+            txtAddress.setText("");
+            txtSalary.setText("");
+        } else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Deleting");
+            alert.setHeaderText(null);
+            alert.setContentText("Empty Fields");
+
+            alert.showAndWait();
+        }
+
+    }
+
+    public void txtIdOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        Customer customer=new CustomerController().searchCustomer(txtId.getText());
+
+        if (null!=customer) {
+            txtId.setText(customer.getId());
+            txtName.setText(customer.getName());
+            txtAddress.setText(customer.getAddress());
+            txtSalary.setText(String.valueOf(customer.getSalary()));
+        } else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Searching");
+            alert.setHeaderText(null);
+            alert.setContentText("Customer Not Found");
+
+            alert.showAndWait();
+        }
+    }
+
+
+}
